b425134bb8e5c39d990fb7732154fa75f4eddf20
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-02-14 22:49:55 +0530

    .

---
+++ php_with-html.php
@@ -0,0 +1,10 @@
+<?php
+$name= "ravisingh";
+echo "<h1 style = 'background-color:red; color:pink;'>my name is : ".$name."</h1";
+?>
+<?php $h2_color="blue";?>
+<h1 style = "color: <?php echo  $h2_color?>">hiii</h1>
+<h1 style = "color: <?php echo  $h2_color?>">hii</h1>
+<h1 style = "color: <?php echo  $h2_color?>">hii</h1>
+<h1 style = "color: <?php echo  $h2_color?>">hii</h1>
+<h1 style = "color: <?php echo  $h2_color?>">hii</h1
